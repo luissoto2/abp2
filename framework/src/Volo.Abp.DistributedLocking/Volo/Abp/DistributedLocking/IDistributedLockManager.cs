276d57a0d85377dbe5b72787c75ce31d5d296156
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-04-26 03:47:38 +0300

    Introduce IDistributedLockManager interface.

---
+++ framework/src/Volo.Abp.DistributedLocking/Volo/Abp/DistributedLocking/IDistributedLockManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Volo.Abp.DistributedLocking
+{
+    public interface IDistributedLockManager
+    {
+        /// <summary>
+        /// Tries to lock a resource.
+        /// </summary>
+        /// <param name="resourceName">The resource name</param>
+        /// <param name="expireTime">
+        /// Expiration time for the lock.
+        /// Keep this expire time small and use <see cref="RefreshLockAsync"/>
+        /// if you need to continue to obtain the lock.
+        /// </param>
+        /// <returns>
+        /// True, if lock is obtained.
+        /// </returns>
+        Task<bool> TryLockAsync(string resourceName, TimeSpan expireTime);
+        
+        /// <summary>
+        /// Refreshes (extends) an already obtained lock.
+        /// </summary>
+        /// <param name="resourceName">The resource name</param>
+        /// <param name="expireTime">
+        /// Expiration time for the lock.
+        /// Keep this expire time small and refresh again
+        /// if you need to continue to obtain the lock.
+        /// </param>
+        Task RefreshLockAsync(string resourceName, TimeSpan expireTime);
+        
+        /// <summary>
+        /// Unlocks a pre-locked resource.
+        /// </summary>
+        /// <param name="resourceName">The resource name</param>
+        Task UnlockAsync(string resourceName);
+    }
+}
